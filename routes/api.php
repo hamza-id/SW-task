80a7b2f3e6f616b697f132a2ef240a0193870629
Hamza Idrees <hamzaidrees41@gmail.com>
2024-03-05 16:43:43 +0500

    route set

--- routes/api.php
+++ routes/api.php
@@ -17,14 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::group([
     'namespace'  => 'App\Http\Controllers\Api',
     'middleware' => 'api',
-    'prefix'     => 'auth'
-
 ], function () {
-    Route::post('/register', 'AuthController@register');
-    Route::post('/login',    'AuthController@login');
+    Route::prefix('auth')->group(function () {
+        Route::post('/register', 'AuthController@register');
+        Route::post('/login',    'AuthController@login');
+    });
 
     Route::middleware('auth:api')->group(function () {
         Route::post('/logout', 'AuthController@logout');
         Route::get('/user',    'AuthController@user');
+
+        Route::apiResource('movies', 'MovieController')->except(['store']);
     });
 });
